78df416238c3128a763b3d9c14f996f0b35dd235
alencion <rhqnrdl625@naver.com>
2020-05-21 16:57:59 +0900

    add settings

---
+++ settings.py
@@ -0,0 +1,30 @@
+import bpy
+from bpy.props import BoolProperty, StringProperty, IntProperty, PointerProperty, FloatProperty
+
+class SlavesWorkSettings(bpy.types.PropertyGroup):
+    
+    @classmethod
+    def register(settings):
+        settings.slaves_work_host = StringProperty(
+            name="Slave's Work Host Ip",
+            description="IP or name of host running local Slaves Work Host",
+            maxlen=180,
+            default="localhost")
+
+        settings.slaves_work_port = IntProperty(
+            name="Slave's Work Host Port",
+            description="TCP port the local Slave's Work Host listens on",
+            default=8081,
+            min=1,
+            max=65535)
+
+        settings.boost_factor = FloatProperty(
+            name="Boost factor",
+            description="Makes rendering faster (and more expensive) by making tiles smaller than they need to be",
+            default=1.0,
+            min=1.0,
+            max=64.0,
+            precision=2,
+            subtype='FACTOR')
+
+        bpy.types.Scene.slaves_work_settings = PointerProperty(type=SlavesWorkSettings, name="Slave's Work Settings", description="Settings for using the Slave's Work service")
--- settings_panel.py
+++ settings_panel.py
@@ -9,12 +9,23 @@ class SlavesWorkPanel(bpy.types.Panel):
     bl_context = "render"
 
     def draw(self, context):
-        # check = isOpenSlavesWork()
+        settings = context.scene.slaves_work_settings
+        self.layout.label(text="Slaves's work Checker",icon='INFO')
+        layout = self.layout.column()
+        row = layout.split(factor=0.5)
+        row.label(text="Slave's Work host:")
+        row.prop(settings, "slaves_work_host", text="")
+        row = layout.split(factor=0.5)
+        row.label(text="Slave's Work port:")
+        row.prop(settings, "slaves_work_port", text="")
+        row = self.layout.row()
+        row.operator("slaves_work.is_running")
+        
         self.layout.separator()
         
         self.layout.label(text="Slaves's work is not working",icon='ERROR')
         row = self.layout.row(align=True)
-        row.operator("slaves_work.render", text='Render', icon='PLAY')
+        row.operator("slaves_work.render", icon='PLAY')
         row.operator("slaves_work.stop", icon='X')
         row = self.layout.row()
         
@@ -42,4 +53,13 @@ class StopSlavesWorkOperator(bpy.types.Operator):
     
     def execute(self, context):
         print("Stop")
+        return {'FINISHED'}
+
+class SlavesWorkChecker(bpy.types.Operator):
+    """Slave's Work Rendering Stop"""
+    bl_idname = "slaves_work.is_running"
+    bl_label = "Running Check"
+    
+    def execute(self, context):
+        print("Checker")
         return {'FINISHED'}
